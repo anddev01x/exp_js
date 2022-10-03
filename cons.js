9943b585025b9ae9092d53be4574e233683c4d19
Trung chieu <nhvk150@gmail.com>
2022-10-03 12:28:34 +0700

    first commit

---
+++ cons.js
@@ -0,0 +1,9 @@
+// example obj 
+const person = {
+    name: "Chieu",
+    age: 21,
+    eyeColor: "black",
+    address: "Vinh"
+  };
+  document.getElementById("exp_js").innerHTML =
+  person.name + " is " + person.age + " years old " + ", live in "+ person.address;
